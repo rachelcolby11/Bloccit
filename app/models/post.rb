12669967b8c2e72b15e69b65fe34c7b798b6520b
Rachel Colby <rachelcolby11@gmail.com>
2015-03-26 20:55:53 -0700

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,10 @@ class Post < ActiveRecord::Base
 
     default_scope { order('created_at DESC') }
 
+    #An ordered_by_title scope.
+    scope :ordered_by_title, -> { reorder('title') }
+
+    #An ordered_by_reverse_created_at scope.
+    scope :ordered_by_reverse_created_at, -> { reorder('created_at') }
+
 end
