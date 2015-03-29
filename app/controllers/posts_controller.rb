79883c9795674a096e7f8f78de2537f41cbf586a
Rachel Colby <rachelcolby11@gmail.com>
2015-03-29 15:36:09 -0700

    Updates after nesting topics and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,4 @@
 class PostsController < ApplicationController
-  # def index
-  #   @posts = Post.all
-  #   authorize @posts
-  # end
 
   def show
     @topic = Topic.find(params[:topic_id])
@@ -21,7 +17,7 @@ class PostsController < ApplicationController
        authorize @post
      if @post.save
        flash[:notice] = "Post was saved."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :new
@@ -40,7 +36,7 @@ class PostsController < ApplicationController
      authorize @post
      if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
