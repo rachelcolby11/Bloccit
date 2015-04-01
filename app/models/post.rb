2dfe547893c226f93bfc29878aa7644a0b86de8a
Rachel Colby <rachelcolby11@gmail.com>
2015-04-01 16:32:45 -0700

    Complete post markdown assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,21 @@ class Post < ActiveRecord::Base
     validates :topic, presence: true
     validates :user, presence: true
 
+  def markdown_title(title)
+    render_as_markdown @post.title
+  end
+
+  def markdown_body(body)
+    render_as_markdown @post.body
+  end
+
+  private
+  
+  def render_as_markdown(attribute)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render attribute).html_safe
+  end
+
 end
