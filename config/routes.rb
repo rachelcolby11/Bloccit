d58d9d0303f4b68a80704557ef14b832756855fd
Rachel Colby <rachelcolby11@gmail.com>
2015-04-07 15:40:08 -0700

    Complete destroy assignment - shallow nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'comments/create'
-
   devise_for :users
   resources :users, only: [:update]
+
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index] 
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
 
 get 'about' => 'welcome#about'
